ec8ad2cb3550203f702321266cdb6435369f1737
shijiashuai <jiashuai.shi@qq.com>
2024-07-25 11:25:00 +0800

    [Fix] 修复R计算的运行问题

--- R-analysis/scripts/diversity_analysis.R
+++ R-analysis/scripts/diversity_analysis.R
@@ -1,23 +1,37 @@
-# diversity_analysis.R
-
-# 加载必要的R包
+# 加载所需的 R 包
 library(vegan)
 library(ggplot2)
 
 # 读取输入数据
 args <- commandArgs(trailingOnly = TRUE)
 kraken_report <- args[1]
-data <- read.csv(kraken_report, sep = "\t")
+data <- read.csv(kraken_report, sep = "\t", header = FALSE)
 
-# 假设输入数据中每一行为一个样本，每一列为一个物种的丰度
+# 打印数据结构以进行调试
+print(str(data))
+
+# 将需要的列转换为数值型数据
+data$V1 <- as.numeric(data$V1)
+data$V2 <- as.numeric(data$V2)
+
+# 检查是否存在至少两列数值型列
+if (ncol(data) < 2) {
+  stop("输入数据至少需要包含两列数值型数据，请检查输入文件。")
+}
+
+# 检查是否存在NA值
+if (any(is.na(data$V1)) || any(is.na(data$V2))) {
+  stop("输入数据包含非数值型数据，请检查输入文件。")
+}
+
 # 计算α多样性（Shannon指数）
-alpha_div <- diversity(data, index = "shannon")
+alpha_div <- diversity(data[, c("V1", "V2")], index = "shannon")
 
 # 保存α多样性结果
-write.csv(alpha_div, "alpha_diversity.csv")
+write.csv(alpha_div, "/ResultData/alpha_diversity.csv")
 
 # 计算β多样性（Bray-Curtis距离）
-beta_div <- vegdist(data, method = "bray")
+beta_div <- vegdist(data[, c("V1", "V2")], method = "bray")
 
 # 进行PCA分析
 pca <- prcomp(beta_div)
@@ -30,4 +44,4 @@ pca_plot <- ggplot(pca_df, aes(x = PC1, y = PC2)) +
     labs(title = "PCA of Beta Diversity", x = "PC1", y = "PC2")
 
 # 保存PCA图
-ggsave("beta_diversity_plot.png", pca_plot)
+ggsave("/ResultData/beta_diversity_plot.png", pca_plot)
