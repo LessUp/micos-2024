682d25f5c940e46aefef1901c7e3475a42dc1a34
shijiashuai <jiashuai.shi@qq.com>
2024-07-28 20:09:44 +0800

    fix-1

--- R-analysis/scripts/diversity_analysis.R
+++ R-analysis/scripts/diversity_analysis.R
@@ -6,24 +6,26 @@ library(ggplot2)
 args <- commandArgs(trailingOnly = TRUE)
 kraken_report <- args[1]
 output_path <- args[2]
-
 data <- read.csv(kraken_report, sep = "\t", header = FALSE)
 
 # 打印数据结构以进行调试
 print(str(data))
 
+# 预处理数据：移除非数值型数据行
+data <- data[grepl("^[0-9.]+$", data$V1) & grepl("^[0-9.]+$", data$V2), ]
+
 # 将需要的列转换为数值型数据
 data$V1 <- as.numeric(data$V1)
 data$V2 <- as.numeric(data$V2)
 
+# 再次检查是否存在NA值
+if (any(is.na(data$V1)) || any(is.na(data$V2))) {
+  stop("输入数据包含非数值型数据，请检查输入文件。")
+}
+
 # 检查是否存在至少两列数值型列
 if (ncol(data) < 2) {
   stop("输入数据至少需要包含两列数值型数据，请检查输入文件。")
-}
-
-# 检查是否存在NA值
-if (any(is.na(data$V1)) || any(is.na(data$V2))) {
-  stop("输入数据包含非数值型数据，请检查输入文件。")
 }
 
 # 计算α多样性（Shannon指数）
