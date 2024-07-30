b7541184810ed566a8871b0b2fc887ebb5f4b2ee
shijiashuai <jiashuai.shi@qq.com>
2024-07-30 10:38:49 +0800

    [update] 简化分类合并脚本

--- scripts/merge-tsv.sh
+++ scripts/merge-tsv.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# 检查是否提供了至少一个文件作为参数
-if [ "$#" -lt 1 ]; then
-  echo "Usage: $0 file1.tsv [file2.tsv ...]"
-  exit 1
-fi
-
-# 合并指定的文件并去除重复行
-cat "$@" | awk '!seen[$0]++' > merged_taxonomy.tsv
+# 合并所有文件并去除重复行
+cat *.taxonomy.tsv | awk '!seen[$0]++' > merged_taxonomy.tsv
